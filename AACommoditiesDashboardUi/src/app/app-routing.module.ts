90992b54a0c9c488d808e92b3f9b077f1303320b
Chima <chima.osuji@gmail.com>
2022-04-21 13:47:33 +0100

    remove unused dependencies

--- AACommoditiesDashboardUi/src/app/app-routing.module.ts
+++ AACommoditiesDashboardUi/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([])],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- AACommoditiesDashboardUi/src/app/app.module.ts
+++ AACommoditiesDashboardUi/src/app/app.module.ts
@@ -1,37 +1,15 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ChartsModule, MDBBootstrapModule } from 'angular-bootstrap-md';
-import {MatDatepickerModule} from '@angular/material/datepicker';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatInputModule } from '@angular/material/input';
-import { MomentDateModule } from '@angular/material-moment-adapter';
-import { RecentHistoryComponent } from './recent-history/recent-history.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    RecentHistoryComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    MDBBootstrapModule.forRoot(),
-    ChartsModule,
-    HttpClientModule,
-    FormsModule,
-    BrowserAnimationsModule,
-    MatDatepickerModule,
-    MatFormFieldModule,
-    ReactiveFormsModule,
-    MatNativeDateModule,
-    MatInputModule,
-    MomentDateModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
