d2baf19e514d23234b7a438ec0fbe73e43b4dbfc
Chima <chima.osuji@gmail.com>
2022-04-21 13:54:15 +0100

    remove all models, services from API

--- AA.CommoditiesDashboard.Api/AA.CommoditiesDashboard.Api/Controllers/CommoditiesController.cs
+++ AA.CommoditiesDashboard.Api/AA.CommoditiesDashboard.Api/Controllers/CommoditiesController.cs
@@ -1,25 +1,15 @@
-﻿using AA.CommoditiesDashboard.Interfaces.Providers;
-using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace AA.CommoditiesDashboard.Api.Controllers
 {
     [ApiController]
     public class CommoditiesController
     {
-        private readonly ICommodityProvider _provider;
-
-        public CommoditiesController(ICommodityProvider provider)
+        [HttpGet("")]
+        public IActionResult Get()
         {
-            _provider = provider;
-        }
-
-        [HttpGet]
-        [Route("api/commodities/recent-history")]
-        public async Task<IActionResult> GetRecentHistory()
-        {
-            var history = await _provider.GetRecentHistoryAsync();
-            return new OkObjectResult(history);
+            return new OkObjectResult(DateTime.UtcNow.ToString());
         }
     }
 }
